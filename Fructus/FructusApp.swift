fddcf0b0b3e9a728ef14a69f015d51d6f32115f9
Jonathan Meriño B <jonathan.merino@ceiba.com.co>
2023-03-07 10:09:21 -0500

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Jonathan Eduardo Meriño Bolívar - Ceiba Software on 7/03/23.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
