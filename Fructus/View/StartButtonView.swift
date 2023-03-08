94c0af2eccdad87db04939a5f974db1baa7d0c32
Jonathan Meriño B <jonathan.merino@ceiba.com.co>
2023-03-08 09:16:56 -0500

    AppStorage

--- Fructus/View/StartButtonView.swift
+++ Fructus/View/StartButtonView.swift
@@ -9,11 +9,11 @@ import SwiftUI
 
 struct StartButtonView: View {
     // MARK: - PROPERTIES
-    
+    @AppStorage("isOnboarding") var isOnboarding: Bool?
     // MARK: - BODY
     var body: some View {
         Button(action: {
-            print("Exit the onboading")
+            isOnboarding = false
         }) {
             HStack(spacing: 8) {
                 Text("Start")
